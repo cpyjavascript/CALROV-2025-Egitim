66059691e26a3d76dc3f5e34c3718d203064c66e
AhmetNihat-171 <ahmetnihatrov171@gmail.com>
2024-09-27 21:35:18 +0300

    Homework

--- ahmet_nihat_sevim.py
+++ ahmet_nihat_sevim.py
@@ -4,28 +4,28 @@ import logging
 import time
 
 logger = logging.getLogger(__name__)
-connection = mavutil.mavlink_connection("/dev/ttyACM0")
-
-connection.wait_heartbeat()
+conn = mavutil.mavlink_connection('udp:127.0.0.1:14550')
 
 class MainClass():
     def __init__(self, logger, connection):
         self.logger = logger
-        self.connection = connection.recv_match(blocking = True).to_dict()
+        self.connection = connection
 
     def extractHeartbeat(self):
-        heartbeat_value = self.connection["HEARTBEAT"]
+        #heartbeat_value = self.connection.get("mavpackettype")
 
-        self.logger.warning(heartbeat_value)
+        self.logger.warning(connection)
     
     def extractAttitude(self):
-        attitude_value = self.connection["ATTITUDE"]
+        #attitude_value = self.connection.get("mavpackettype")
 
-        self.logger.warning(attitude_value)
-    
-classObject = MainClass(logger = logger, connection = connection)
+        self.logger.warning(connection)
 
 while True:
+    connection = conn.recv_match(blocking = True).to_dict()
+
+    classObject = MainClass(logger = logger, connection = connection)
+
     thread_heartbeat = Thread(target = classObject.extractHeartbeat)
     thread_attitude = Thread(target = classObject.extractAttitude)
 
