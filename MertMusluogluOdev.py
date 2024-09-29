1fbe9131fb1634104b101ab03a1d7633905f4ede
Mert MUSLUOĞLU <musluoglu.mert10@gmail.com>
2024-09-29 16:59:36 +0300

    Add files via upload

---
+++ MertMusluogluOdev.py
@@ -0,0 +1,49 @@
+from pymavlink import mavutil
+from threading import Thread
+from time import sleep
+import logging
+
+aktifMi = True
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Mert, hata alırsan "udp" adresine bak muhtemelen yanlış girmişsindir.
+mavlinkBaglantisi = mavutil.mavlink_connection('udp:127.0.0.1:14550')
+if mavlinkBaglantisi:
+    logger.info("Bağlantı başarılı!")
+else:
+    logger.error("Bağlantı başarısız!")
+
+mavlinkBaglantisi.wait_heartbeat()
+
+class aksiyonlar:
+    def __init__(self, logger, mesaj):
+        self.logger = logger
+        self.mesaj = mesaj
+
+
+    def irtifaYazdır(self):
+        if hasattr(self.mesaj, 'alt'):
+            irtifa = self.mesaj.alt
+            self.logger.info(f"İrtifa: {irtifa} metre")
+        # Mert, çıktında olmadık yerde "metre" yazısı varsa yukardakini sil boş boş bakma koda.
+
+    def heartbYazdır(self):
+        if hasattr(self.heartbYazdır, 'HEARTBEAT'):
+            logger.info(mavlinkBaglantisi.recv_match(type="HEARTBEAT",blocking=True))
+      
+while aktifMi:
+    mesaj = mavlinkBaglantisi.recv_match(blocking=True)
+    print(f"Gelen mesaj: {mesaj}")
+    
+    actionObjects = aksiyonlar(logger, mesaj)
+
+    irtifa_thread = Thread(target= actionObjects.irtifaYazdır)
+    heartbeat_thread = Thread(target= actionObjects.heartbYazdır)
+
+    irtifa_thread.start()
+    heartbeat_thread.start()
+    
+    irtifa_thread.join()
+    heartbeat_thread.join()
+    sleep(0.5)
